d4d3a63b4379ae4b7478612f60bd938542d762bd
Karan Bhatia <karanb@Karans-Laptop.local>
2024-05-15 14:33:12 -0400

    app start up

---
+++ Controllers/CityController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jubilado.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet("{cityName}")]
+        public JsonResult Get(string cityName)
+        {
+            return new JsonResult("Foo");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,47 @@
+
+/*
+* STEPS:
+* Get app running locally
+* Move this route to controller
+* Create city class and put city object
+* Create route to get city info
+* Download Postman to do the same
+* Create route to put weather history
+* Create route to get weather history
+* Create a script to Put multiple city and weather info's
+* ...
+*/
+
 var builder = WebApplication.CreateBuilder(args);
+Console.WriteLine("here");
+// Add services to the container.
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseRouting();
+
+app.MapGet("/foo", async context =>
+{
+    await context.Response.WriteAsync("Hello World!");
+});
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
